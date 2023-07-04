a61bb60bc96a3625ce459ee056ad6ee603495c73
tbagaciunas <tomas1089@gmail.com>
2023-07-04 22:43:39 +0300

    home update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Home from "./pages/Home";
 import "./App.css";
+import "./index.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,24 @@
 import React from "react";
+import "../App.css";
 
-function Navbar() {
+function Home() {
   return (
     <div className="home-header">
-      <div className="home-container"></div>
+      <div className="home-container">
+        <h1>
+          Get your vehicle <br />
+          service done online <br />
+          at one place
+        </h1>
+      </div>
+      <div className="image-container">
+        <img className="homeimg" src="image1.png" alt="homeimg" />
+        <div className="rating">
+          <img src="rating.png" alt="rating" />
+        </div>
+      </div>
     </div>
   );
 }
 
-export default Navbar;
+export default Home;
