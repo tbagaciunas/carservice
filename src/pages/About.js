a7194589030d1bd4319e468171fe21573e0bec84
tbagaciunas <tomas1089@gmail.com>
2023-07-10 20:56:59 +0300

    about us initiated

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,25 @@
 import React from "react";
+import "../App.css";
+import Button from "../components/Button";
 
 function About() {
-  return <h1>about Page</h1>;
+  return (
+    <div>
+      <div className="our-services-container">
+        <img className="about-img" src="about.png" alt="about" />
+        <div className="box">
+          <h1>About us</h1>
+          <p>
+            Through True Rich Attended does no end it his mother since <br></br>{" "}
+            real had half every him case in packages enquire we up ecstatic{" "}
+            <br></br>
+            unsatiable saw Through True Rich Attended does
+          </p>
+          <Button text="Book a service" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
